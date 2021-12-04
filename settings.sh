20424679a42468db0b2d7b8f272af13cd97fa8e4
Robert Maratos <robertmaratos@gmail.com>
2021-12-04 05:28:43 +0000

    more aliases and settings

--- settings.sh
+++ settings.sh
@@ -1,1 +1,15 @@
+# shell
+
+# KEEP COMMAND HISTORY
+HISTSIZE=10000000
+SAVEHIST=10000000
+
+# git pull --rebase
 git config pull.rebase true
+
+# iTerm2 Title
+# https://gist.github.com/phette23/5270658
+DISABLE_AUTO_TITLE="true"
+
+# include node_modules/.bin in PATH
+export PATH="$(yarn global bin):$(yarn bin):$PATH"
